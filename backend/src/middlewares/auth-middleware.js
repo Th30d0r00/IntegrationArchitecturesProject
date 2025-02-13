78eb6b8515f6aa54291e0be35cba4df0744b1451
Jonas Roemmer-Collmann <jonas.roemmer-collmann@smail.inf.h-brs.de>
2025-02-13 17:44:51 +0100

    feat: add basic api-security

--- backend/src/middlewares/auth-middleware.js
+++ backend/src/middlewares/auth-middleware.js
@@ -5,13 +5,48 @@
  * @return {(function(*, *, *): void)|*}
  */
 exports.checkAuthorization = (beAdmin) => {
-    return (req, res, next) => {
-        if(req.session.authenticated){ //check if session was marked as authenticated
-            if(!beAdmin || req.session.user.isAdmin){ //check if admin-requirement is met
-                next(); //proceed with next middleware or handler
-                return;
-            }
-        }
-        res.status(401).send(); //intercept request
+  return (req, res, next) => {
+    console.log(req.session);
+    if (req.session.authenticated) {
+      //check if session was marked as authenticated
+      if (!beAdmin || req.session.user.isAdmin) {
+        //check if admin-requirement is met
+        next(); //proceed with next middleware or handler
+        return;
+      }
     }
-}
+    res.status(401).send(); //intercept request
+  };
+};
+
+/**
+ * this express middleware checks if a user is authenticated or even has admin permissions;
+ * otherwise the request gets intercepted and status 401 is returned
+ * @param {string[]} roles the roles that are required
+ * @param {string} roleWithSid the role that is required and must match the session user's sid
+ * @return {(function(*, *, *): void)|*}
+ */
+exports.checkAuthorizationWithRolesAndSid = (roles, roleWithSid) => {
+  return (req, res, next) => {
+    if (req.session.authenticated) {
+      //check if session was marked as authenticated
+      console.log(
+        req.session.user.role,
+        roleWithSid,
+        req.session.user.sid,
+        req.params.sid
+      );
+      if (
+        roles.includes(req.session.user.role) &&
+        (!(req.session.user.role === roleWithSid) ||
+          (req.session.user.role === roleWithSid &&
+            req.session.user.sid == req.params.sid))
+      ) {
+        //check if admin-requirement is met
+        next(); //proceed with next middleware or handler
+        return;
+      }
+    }
+    res.status(401).send(); //intercept request
+  };
+};
--- frontend/src/app/services/salesmen.service.ts
+++ frontend/src/app/services/salesmen.service.ts
@@ -1,78 +1,121 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
-import {environment} from '../../../environments/environment';
-import {Observable} from 'rxjs';
-import {SalesmenDatapoint} from '../interfaces/salesmen-datapoint';
-import {PerformanceDatapoint} from '../interfaces/performance-datapoint';
-import {Competence} from '../models/Competence';
-import {SalesmenDatapointWithPerformanceYear} from '../interfaces/salesmen-datapoint-performance-year';
-import {ProductSalesDatapoint} from '../interfaces/productsSales-datapoint';
-import {ApprovalStatus} from '../models/Approval-status';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { SalesmenDatapoint } from '../interfaces/salesmen-datapoint';
+import { PerformanceDatapoint } from '../interfaces/performance-datapoint';
+import { Competence } from '../models/Competence';
+import { SalesmenDatapointWithPerformanceYear } from '../interfaces/salesmen-datapoint-performance-year';
+import { ProductSalesDatapoint } from '../interfaces/productsSales-datapoint';
+import { ApprovalStatus } from '../models/Approval-status';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class SalesmenService {
-
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {}
 
     getSalesmen(): Observable<HttpResponse<SalesmenDatapoint[]>> {
         const url = `${environment.apiEndpoint}/api/salesmen`;
-        return this.http.get<SalesmenDatapoint[]>(url, { observe: 'response', withCredentials: true });
+        return this.http.get<SalesmenDatapoint[]>(url, {
+            observe: 'response',
+            withCredentials: true,
+        });
     }
 
     getSalesmanById(sid: number): Observable<HttpResponse<SalesmenDatapoint>> {
         const url = `${environment.apiEndpoint}/api/salesmen/${sid}`;
-        return this.http.get<SalesmenDatapoint>(url, { observe: 'response', withCredentials: true });
-    }
-
-    getPerformanceRecords(sid: number): Observable<HttpResponse<PerformanceDatapoint[]>> {
-        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance`;
-        return this.http.get<PerformanceDatapoint[]>(url, { observe: 'response', withCredentials: true });
-    }
-
-    getSalesmanPerformanceByYear(sid: number, year: number): Observable<HttpResponse<PerformanceDatapoint>> {
-        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance/${year}`;
-        return this.http.get<PerformanceDatapoint>(url, { observe: 'response', withCredentials: true });
-    }
-
-    getApprovedPerformanceRecords(sid: number): Observable<HttpResponse<PerformanceDatapoint[]>> {
-        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance/approved`;
-        return this.http.get<PerformanceDatapoint[]>(url, { observe: 'response', withCredentials: true });
-    }
-
-    addPerformanceRecord(sid: number, record: { sid: number; year: number; competences: Competence[] }): Observable<HttpResponse<any>> {
-        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance`;
-        return this.http.post<any>(url, record, { observe: 'response' });
-    }
-
-    updatePerformanceRecord(
-        sid: number, record: { sid: number; year: number; competences: Competence[] }): Observable<HttpResponse<any>> {
-        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance`;
-        return this.http.put<any>(url, record, { observe: 'response' });
-    }
-
-    getSalesmenWithUnapprovedRecords(): Observable<HttpResponse<SalesmenDatapointWithPerformanceYear[]>> {
-        const url = `${environment.apiEndpoint}/api/unapprovedSalesmenRecords`;
-        return this.http.get<SalesmenDatapointWithPerformanceYear[]>(url, { observe: 'response', withCredentials: true });
-    }
-
-    updateApprovalStatusPerformanceRecord(
-        sid: number, year: number, approvalStatus: ApprovalStatus, remark: string): Observable<HttpResponse<any>> {
-        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance/${year}`;
-        const record = { approvalStatus, remark };
-        return this.http.put<any>(url, record, { observe: 'response' });
-    }
-
-    getSalesOrdersByGovernmentIdAndYear(gid: number, year: number): Observable<HttpResponse<ProductSalesDatapoint[]>> {
-        const url = `${environment.apiEndpoint}/api/salesorders/${gid}/${year}`;
-        return this.http.get<ProductSalesDatapoint[]>(url, {
+        return this.http.get<SalesmenDatapoint>(url, {
             observe: 'response',
-            withCredentials: true
+            withCredentials: true,
         });
     }
 
+    getPerformanceRecords(
+        sid: number
+    ): Observable<HttpResponse<PerformanceDatapoint[]>> {
+        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance`;
+        return this.http.get<PerformanceDatapoint[]>(url, {
+            observe: 'response',
+            withCredentials: true,
+        });
+    }
 
+    getSalesmanPerformanceByYear(
+        sid: number,
+        year: number
+    ): Observable<HttpResponse<PerformanceDatapoint>> {
+        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance/${year}`;
+        return this.http.get<PerformanceDatapoint>(url, {
+            observe: 'response',
+            withCredentials: true,
+        });
+    }
 
+    getApprovedPerformanceRecords(
+        sid: number
+    ): Observable<HttpResponse<PerformanceDatapoint[]>> {
+        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance/approved`;
+        return this.http.get<PerformanceDatapoint[]>(url, {
+            observe: 'response',
+            withCredentials: true,
+        });
+    }
 
+    addPerformanceRecord(
+        sid: number,
+        record: { sid: number; year: number; competences: Competence[] }
+    ): Observable<HttpResponse<any>> {
+        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance`;
+        return this.http.post<any>(url, record, {
+            observe: 'response',
+            withCredentials: true,
+        });
+    }
+
+    updatePerformanceRecord(
+        sid: number,
+        record: { sid: number; year: number; competences: Competence[] }
+    ): Observable<HttpResponse<any>> {
+        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance`;
+        return this.http.put<any>(url, record, {
+            observe: 'response',
+            withCredentials: true,
+        });
+    }
+
+    getSalesmenWithUnapprovedRecords(): Observable<
+        HttpResponse<SalesmenDatapointWithPerformanceYear[]>
+    > {
+        const url = `${environment.apiEndpoint}/api/unapprovedSalesmenRecords`;
+        return this.http.get<SalesmenDatapointWithPerformanceYear[]>(url, {
+            observe: 'response',
+            withCredentials: true,
+        });
+    }
+
+    updateApprovalStatusPerformanceRecord(
+        sid: number,
+        year: number,
+        approvalStatus: ApprovalStatus,
+        remark: string
+    ): Observable<HttpResponse<any>> {
+        const url = `${environment.apiEndpoint}/api/salesmen/${sid}/performance/${year}`;
+        const record = { approvalStatus, remark };
+        return this.http.put<any>(url, record, {
+            observe: 'response',
+            withCredentials: true,
+        });
+    }
+
+    getSalesOrdersByGovernmentIdAndYear(
+        gid: number,
+        year: number
+    ): Observable<HttpResponse<ProductSalesDatapoint[]>> {
+        const url = `${environment.apiEndpoint}/api/salesorders/${gid}/${year}`;
+        return this.http.get<ProductSalesDatapoint[]>(url, {
+            observe: 'response',
+            withCredentials: true,
+        });
+    }
 }
